ba8fa609d0a8316bdab957223331e34bb318121f
Levi Sousa Barros <levi-sousabarros@hotmail.com>
2025-02-20 22:23:45 -0300

    atualizacao2002

--- cypress/e2e/loja_ebac/login.cy.js
+++ cypress/e2e/loja_ebac/login.cy.js
@@ -45,7 +45,7 @@ describe( 'Funcionalidade : Login' ,   () => {
     });
 
 
-    it.only('Deve fazer login com sucesso Usando massa de dados', () => {
+    it('Deve fazer login com sucesso Usando massa de dados', () => {
         cy.get('#username').type(perfil.usuario)
         cy.get('#password').type(perfil.senha)
         cy.get('.woocommerce-form > .button').click()              
@@ -54,8 +54,15 @@ describe( 'Funcionalidade : Login' ,   () => {
 
     });
 
-    it('Deve fazer login com sucesso - Usando Fixture', () => {
-        cy.fixture('perfil')
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then( dados =>  {
+            cy.get('#username').type(dados.usuario , {log: false})
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()              
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, levi.barros (não é levi.barros? Sair)')
+
+
+        })
 
 
     });
